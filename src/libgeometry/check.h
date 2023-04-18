94c398a1232de2cb95314a0c412d824c25fe1874
Rouva Huu <ulyana.bahc2015@yandex.ru>
2023-04-18 16:55:23 +0700

    Created a file with a function call

---
+++ src/libgeometry/check.h
@@ -0,0 +1,13 @@
+void check_str(char *a, char *b, int *ind_open_bracket, int *error);
+
+void check_open_bracket(char *a, int l, int *ind_close_bracket);
+
+void check_first_num(char *a, int *ind_open_bracket, int *error, int *ind_first_num_elm);
+
+void check_second_num(char *a, int *ind_first_num_elm, int *ind_second_num_elm, int *error);
+    
+void check_last_num(char *a, int *ind_second_num_elm, int *ind_close_bracket, int *error, int *ind_last_num_elm);
+        
+void check_close_bracket(char *a, int l, int *ind_last_num_elm, int *error, int *ind_close_bracket);
+
+void check_unexp_token(char *a, int l, int *ind_close_bracket, int *error);
